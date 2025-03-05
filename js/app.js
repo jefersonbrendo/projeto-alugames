12e9b08d5858d386fe3bf99d4d3b55bf744de331
jefersonbrendo <jefersonbrendo077@gmail.com>
2025-03-05 00:25:47 -0300

    otimizar codigo

--- js/app.js
+++ js/app.js
@@ -4,35 +4,17 @@ const classeBotaoAlugada = 'dashboard__item__button--return';
 const classeBotao = 'dashboard__item__button';
 
 function alterarStatus(id) {
-    alterarStatusImagem(id); 
-    alterarStatusBotao(id);
-}
-
-function alterarStatusImagem(id) {
     let jogo = document.getElementById(`game-${id}`);
-    let statusImagem = jogo.querySelector(`.${classeImagem}`);
+    let imagem = jogo.querySelector(`.${classeImagem}`);
+    let botao = jogo.querySelector(`.${classeBotao}`);
 
-    if (statusImagem.classList.contains(classeImagem && classeImagemAlugada)) {
-        statusImagem.classList.remove(classeImagem && classeImagemAlugada);
-        statusImagem.classList.add(classeImagem);
+    if (imagem.classList.contains(classeImagemAlugada)) {
+        imagem.classList.remove(classeImagemAlugada);
+        botao.classList.remove(classeBotaoAlugada);
+        botao.innerHTML = 'Alugar';
     } else {
-        statusImagem.classList.remove(classeImagem && classeImagemAlugada);
-        statusImagem.classList.add(classeImagem && classeImagemAlugada);
-    }
-} 
-
-function alterarStatusBotao(id) {
-    let jogo = document.getElementById(`game-${id}`);
-    let statusBotao = jogo.querySelector(`.${classeBotao}`);
-    console.log(statusBotao.classList);
-
-    if (statusBotao.classList.contains(classeBotao && classeBotaoAlugada)) {
-        statusBotao.classList.remove(classeBotao && classeBotaoAlugada);
-        statusBotao.classList.add(classeBotao);
-        statusBotao.innerHTML = 'Alugar';
-    } else {
-        statusBotao.classList.remove(classeBotao && classeBotaoAlugada);
-        statusBotao.classList.add(classeBotao && classeBotaoAlugada);
-        statusBotao.innerHTML = 'Devolver';
+        imagem.classList.add(classeImagemAlugada);
+        botao.classList.add(classeBotaoAlugada);
+        botao.innerHTML = 'Devolver';
     }
 }
